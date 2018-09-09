cecb5c4a8722bf6644b7c98762b09d024e6b370e
annavu <anna.wrzos@gmail.com>
2018-09-09 19:55:25 +0200

    create file with variables(content)

---
+++ src/js/content-var.js
@@ -0,0 +1,181 @@
+export const servicesContent =  `
+<p class="title text-center">Services</p>
+<div class="row">
+  <div class="card col-md-3 col-sm-6 my-3 text-center">
+    <div class="card__image mb-1">
+      <i class="fas fa-project-diagram"></i>
+    </div>
+    <div class="card__content py-3">
+        <h6 class="card__title font-weight-bold">Design</h6>
+        <p class="card__text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia... </p>
+    </div>
+  </div>
+
+  <div class="card col-md-3 col-sm-6 my-3 text-center">
+    <div class="card__image mb-1">
+      <i class="fas fa-laptop-code"></i>
+    </div>
+    <div class="card__content py-3">
+      <h6 class="card__title font-weight-bold">Development</h6>
+      <p class="card__text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia... </p>
+    </div>
+  </div>
+
+  <div class="card col-md-3 col-sm-6 my-3 text-center">
+    <div class="card__image mb-1">
+      <i class="fas fa-chart-line"></i>
+    </div>
+    <div class="card__content py-3">
+      <h6 class="card__title font-weight-bold">Optimization</h6>
+      <p class="card__text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia... </p>
+    </div>
+  </div>
+        
+  <div class="card col-md-3 col-sm-6 my-3 text-center">
+    <div class="card__image mb-1">
+      <i class="fas fa-cogs"></i>
+    </div>
+    <div class="card__content py-3">
+      <h6 class="card__title font-weight-bold">Support</h6>
+      <p class="card__text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia... </p>
+    </div>
+  </div>          
+</div>
+<p class="link">Check more</p>
+`;
+
+export const teamContent = `
+<p class="title">Team</p>
+<div class="row mt-1">
+  <div class="team col-md-3 col-sm-6 my-3">
+    <div class="team__image mb-3 m-auto">
+      <img src="img/team2.png" class="img-fluid" alt="">
+    </div>
+    <div class="team__content">
+      <p class="team__title p-0 m-0 mt-3">Kate Pris</p>
+      <p class="team__text p-0 mb-2 text-uppercase">Managing Director</p>
+      <img  class="team__icon mr-1" src="img/fb.png" alt="">
+      <img class="team__icon mr-1" src="img/tw.png" alt="">
+      <img class="team__icon" src="img/g.png" alt="">
+    </div>
+  </div>
+    
+  <div class="team col-md-3 col-sm-6 my-3">
+    <div class="team__image mb-3 m-auto">
+      <img src="img/team3.png" class="img-fluid" alt="">
+    </div>
+    <div class="team__content">
+      <p class="team__title p-0 m-0 mt-3">John Warden</p>
+      <p class="team__text p-0 mb-2 text-uppercase">Lead Developer</p>
+      <img  class="team__icon mr-1" src="img/fb.png" alt="">
+      <img class="team__icon mr-1" src="img/tw.png" alt="">
+      <img class="team__icon" src="img/g.png" alt="">
+    </div>
+  </div>
+    
+  <div class="team col-md-3 col-sm-6 my-3">
+    <div class="team__image mb-3 m-auto">
+      <img src="img/team4.png" class="img-fluid" alt="">
+    </div>
+    <div class="team__content">
+      <p class="team__title p-0 m-0 mt-3">Mark Smith</p>
+      <p class="team__text p-0 mb-2 text-uppercase">Engineering</p>
+      <img  class="team__icon mr-1" src="img/fb.png" alt="">
+      <img class="team__icon mr-1" src="img/tw.png" alt="">
+      <img class="team__icon" src="img/g.png" alt="">
+    </div>
+  </div>
+    
+  <div class="team col-md-3 col-sm-6 my-3">
+    <div class="team__image mb-3 m-auto">
+      <img src="img/team2.png" class="img-fluid" alt="">
+    </div>
+    <div class="team__content">
+      <p class="team__title p-0 m-0 mt-3">Mary Gail</p>
+      <p class="team__text p-0 mb-2 text-uppercase">Lead Designer</p>
+      <img  class="team__icon mr-1" src="img/fb.png" alt="">
+      <img class="team__icon mr-1" src="img/tw.png" alt="">
+      <img class="team__icon" src="img/g.png" alt="">
+    </div>
+  </div>
+</div>
+`;
+
+
+export const worksContent = `
+<div class="container text-center title">
+<p class="title">Our Works</p>
+  <div class="row row--works m-auto">
+    <div class="image image--bg1 col-md-3 col-sm-4">
+      <div class="overlay"></div>
+    </div>
+    <div class="image image--bg2 d-none d-md-block col-md-3 col-sm-4 ">
+      <div class="overlay"></div>
+    </div>
+    <div class="image image--bg3 col-md-3 col-sm-4">
+      <div class="overlay"></div>
+    </div>
+    <div class="image image--bg4 d-none d-md-block col-md-3 col-sm-4">
+      <div class="overlay"></div>
+    </div>
+    <div class="image image--bg5 col-md-3 col-sm-4">
+      <div class="overlay--active"></div>
+    </div>
+    <div class="image image--bg6 col-md-3 col-sm-4">
+      <div class="overlay"></div>
+    </div>
+    <div class="image image--bg7 col-md-3 col-sm-4">
+      <div class="overlay"></div>
+    </div>
+    <div class="image image--bg8 col-md-3 col-sm-4">
+      <div class="overlay"></div>
+    </div>
+    <div class="image image--bg9 d-none d-md-block col-md-3 col-sm-4">
+      <div class="overlay"></div>
+    </div>
+    <div class="image image--bg10 d-none d-md-block col-md-3 col-sm-4">
+      <div class="overlay"></div>
+    </div>
+    <div class="image image--bg11 d-none d-md-block col-md-3 col-sm-4">
+      <div class="overlay"></div>
+    </div>
+    <div class="image image--bg12 d-none d-md-block col-md-3 col-sm-4">
+      <div class="overlay"></div>
+    </div>
+  </div>
+</div>
+`;
+
+
+export const contactContent = `
+<div class="container text-center py-5">
+  <p class="title">Say Hello</p>
+  <form>
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-4">
+        <input type="text" class="form-control mt-3 mb-1 input" placeholder="Your email..">
+      </div>
+      <div class="col-md-4">
+        <input type="text" class="form-control mt-3 mb-1 input" placeholder="Subject..">
+      </div>
+    </div>
+  </form>
+  <div class="row d-flex justify-content-center">
+    <div class="col-md-8">
+      <div class="form-group mt-5">
+        <textarea  class="form-control" rows="7" placeholder="Your message.."></textarea>
+      </div>
+      <div class="form-group d-flex justify-content-between">
+        <button class="btn my-3 px-3">Send</button>
+        <div class="socials my-3 d-none d-md-block">
+          <img src="img/face.png" alt="" class="socials__icon px-1">
+          <img src="img/twitter.png" alt="" class="socials__icon px-1">
+          <img src="img/google.png" alt="" class="socials__icon px-1">
+          <img src="img/dribbble.png" alt="" class="socials__icon px-1">
+          <img src="img/be.png" alt="" class="socials__icon pl-1">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>      
+`;
